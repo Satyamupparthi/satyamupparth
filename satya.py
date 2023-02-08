83b79cf1d47a4f93fe3818b19bab11455559a49f
Satyamupparthi <110172814+Satyamupparthi@users.noreply.github.com>
2023-02-08 22:06:55 +0530

    Create satya.py

---
+++ satya.py
@@ -0,0 +1,78 @@
+print(5/2)
+print(5//2)
+print(2*2*2)
+print(2*3)
+print(2**3)
+print(10%3)
+name='satya'
+print(name)
+print(10%1)
+print(10/3)
+print('satya')
+print("satya")
+print("satya's laptop")
+print('satya "laptop"')
+print('satya\s'"laptop")#\s represent it's skip the special meaning of the code
+print('satya'+'satya')
+print(10*'satya')
+print('c:\docs\navya')#\n represent ew line
+print(r'c:\docs\navya')
+#variables
+x=2
+print(x+2)
+y=3
+print(x+y)
+x=9
+print(x+y)
+print(x)
+print(x+10)
+name='satya'
+print(name)
+print(name+'mupparthi')
+print(name[0])
+print(name[4])
+print(name[-1])
+#slicing
+print(name[0:2])
+print(name[1:4])
+print(name[1:])
+print(name[:3])
+print(name[3:10])
+print(name[0:3])
+print('durga'+name[0:4])
+myname='satya'
+print(len(myname))#len function represent length of the string
+#
+num=[25,12,36,95,14]
+print(num[0])
+print(num[4])
+print(num[2:])#slicing
+print(num[-1])
+print(num[-4])
+name=['satya','durga','surya']
+print(name)
+value=[9.5,'satya',25]
+print(name,value)
+mil=[num,name]
+print(mil)
+num.append(45)# append function represent adding new element in to list
+print(num)
+num.insert(2,77)#insert function represent the inserting the values in list withe using index position
+print(num)
+num.remove(14)
+print(num)
+num.pop(1)
+print(num)
+num.pop()
+print(num)
+del num[2:]
+print(num)
+num.clear()#clear function represent clear the elements in list
+print(num)
+num.extend([29,12,14,36,89])
+print(num)
+print(min(num))
+print(max(num))
+print(sum(num))
+num.sort()
+print(num)
